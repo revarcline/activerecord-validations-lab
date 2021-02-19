537417c405dbbd9ba06c4d41607e1928913c801d
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-18 23:02:52 -0600

    commit tree d52f5e44708d27e54a7d9f828074ec7e9b5404df

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :is_clickbait
+
+  def is_clickbait
+    errors.add(:title, 'must be clickbait') unless title =~ /(Won't\ Believe|Secret|Top \d|Guess)/i
+  end
 end
